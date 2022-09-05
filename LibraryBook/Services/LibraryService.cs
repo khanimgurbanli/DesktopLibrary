4b004b42fcd32adb2965dc4db2e3cda6950ec254
khanimgurbanli <zeri.q.222@gmail.com>
2022-09-04 21:43:05 -0700

    change commit

--- LibraryBook/Services/LibraryService.cs
+++ LibraryBook/Services/LibraryService.cs
@@ -1,72 +1,91 @@
-﻿using LibraryBook.IServices;
+﻿using LibraryBook.Base;
+using LibraryBook.Data;
+using LibraryBook.IServices;
 using LibraryBook.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace LibraryBook.Services
 {
-    public class LibraryService : IlibraryService
+    public class LibraryService : Repository<Book>, ILibraryService
     {
-        ErrorLogger errorLogger = new ErrorLogger();
-        private readonly IBookService _dbBooks;
-        public LibraryService(IBookService books)
+        //public LibraryService() { }
+        //private readonly IRepository<Book> _repository;
+        //public LibraryService(IRepository<Book> repository) => _repository = repository;
+        Repository<Book> _repository = new Repository<Book>();
+        public Book AddBook(params string[] values)
         {
-            this._dbBooks = books;   
-        }
+            //Create a new book]
+            Book addBook = new Book();
 
-        public List<Book> FindAllBooksByNAME(string bookName)
+            addBook.Name = values[0];
+            addBook.AuthorName = values[1];
+            addBook.PageCount = int.Parse(values[2]);
+            addBook.Code = (values[0] as string).GenerateCode();
+            addBook.Price = double.Parse(values[3]);
+
+            //And add a new book to list
+                _repository.Add(addBook);
+
+
+            //Show message about added book
+            Console.WriteLine("Add a new book");
+            return addBook;
+        }
+        public Book GetBook(string search)
         {
-            var findBook = _dbBooks.BookList().FindAll(x => x.Name == bookName).ToList();
+            //Search book in list by name, page count, author name or by other parameters 
+            var findBook = _repository.Get((x => x.Name == search
+                                            || x.AuthorName == search
+                                            || x.PageCount == Double.Parse(search)
+                                            || x.Price == double.Parse(search)
+                                            || x.Code == search));
+
+            //If not found record in list show from delegat message
+            if (findBook == null)
+                ErrorLoggerMessage();
+
+            return findBook;
+        }
+        public List<Book> FindAllBooks(string search)
+        {
+            //Found book by name, page count, author name or price in list 
+            var findBook = _repository.FindAll(x => x.Name == search
+                                            || x.AuthorName == search
+                                            || x.PageCount == Double.Parse(search)
+                                            || x.Price == double.Parse(search)
+                                            || x.Code == search);
             return findBook;
         }
 
-        public List<Book> RemoveAllBooksByNAME(string bookName)
+        public int RemoveAllBooks(string search)
         {
-            ErrorLoggDelegate errorLoggDelegate = new ErrorLoggDelegate(errorLogger.FindBookLogger);
-            var findBook = _dbBooks.BookList().FindAll(x => x.Name == bookName).ToList();
+            var findBook = FindAllBooks(search);
 
+          //If not found record in list show from delegate message
             if (findBook == null)
-                errorLoggDelegate.Invoke("Not found record");
+                ErrorLoggerMessage();
 
             foreach (var book in findBook)
-            {
-                _dbBooks.BookList().Remove(book);
-                Console.WriteLine("Remove book");
-            }
-            return findBook;
+                findBook.Remove(book);
+
+            //At last show message about deleted books
+            Console.WriteLine("Remove book");
+            return findBook.Count;
         }
-
-        public void RemoveBooksByCode(string Code)
+        void ErrorLoggerMessage()
         {
-            ErrorLoggDelegate errorLoggDelegate = new ErrorLoggDelegate(errorLogger.FindBookLogger);
-            var findBook = _dbBooks.BookList().FirstOrDefault(x => x.Name == Code);
+            // Create logger class instance for using logger method
+            ErrorLogger errorLogger = new ErrorLogger();
 
-            if (findBook == null)
-                errorLoggDelegate.Invoke("Not found book");
-
-            _dbBooks.BookList().Remove(findBook);
-            Console.WriteLine("Removed all  book ");
-        }
-
-        public List<Book> SearchBooks(string bookInfo)
-        {
-            ErrorLoggDelegate errorLoggDelegate = new ErrorLoggDelegate(errorLogger.FindBookLogger);
-
-            var findBook = _dbBooks.BookList().FindAll(x => x.Name == bookInfo
-            || x.AuthorName == bookInfo
-            || x.PageCount == Double.Parse(bookInfo)).ToList();
-            foreach (var book in findBook)
-            {
-                Console.WriteLine($"\nName: {book.Name}\n Authorname: {book.AuthorName }\n Page count: {book.PageCount}\n Code: {book.Code}");
-            }
-
-            if (findBook == null)
-                errorLoggDelegate.Invoke("Not found book");
-
-            return findBook;
+            //Generate  not found error message by delegate
+            var errorLoggDelegate = new ErrorLoggDelegateHandler(errorLogger.FindBookLogger);
+            errorLoggDelegate.Invoke("Not found book");
         }
     }
 }
+
